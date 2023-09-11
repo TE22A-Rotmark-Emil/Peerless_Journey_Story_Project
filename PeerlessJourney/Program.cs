56898bea6430f6197695b3c47721c7a4d048a8d4
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2023-09-11 09:43:44 +0200

    0.0.0.3 - added Debug and DebugMiller

--- PeerlessJourney/Program.cs
+++ PeerlessJourney/Program.cs
@@ -12,13 +12,14 @@ int pis = names.Count();
 int pis2 = ExcludeList.Count();
 int reputation = 0;
 int insanity = 0;
+int threadSleepfr = 500;
 
 Console.WriteLine($"Welcome, User.");
-Thread.Sleep(2000);
+Thread.Sleep(threadSleepfr * 4);
 Console.WriteLine($"Are you okay with the name '{name}'?");
 string? RawyN = Console.ReadLine();
 string yN = RawyN.ToLower();
-while (yN != "yes" && yN != "no") {
+while (yN != "yes" && yN != "no" && yN != "debug" && yN != "debugmiller") {
     Console.WriteLine($"Invalid Response. Answer must be 'yes' or 'no', not '{yN}'");
     string? inyN = Console.ReadLine();
     if (inyN == "yes" || inyN == "no") {
@@ -28,11 +29,19 @@ while (yN != "yes" && yN != "no") {
         break;
     }
 }
+if (yN == "debug") {
+    threadSleepfr = 1;
+    name = "Frederick";
+}
+if (yN == "debugmiller") {
+    threadSleepfr = 1;
+    name = "Miller Freiburg";
+}
 Console.WriteLine($"> {yN}");
 if (yN == "no") {
     Console.WriteLine("Okay.");
     ExcludeList.Add(name);
-    Thread.Sleep(500);
+    Thread.Sleep(threadSleepfr);
     while (yN == "no" && pis != pis2) {
         int inNameRNG = generator.Next(10);
         var inName = names[inNameRNG];
@@ -70,35 +79,35 @@ if (yN == "no") {
 
 if (pis == pis2) {
     Console.WriteLine("Wow, you really are insufferable.");
-    Thread.Sleep(2500);
+    Thread.Sleep(threadSleepfr*5);
     Console.WriteLine("I gave you");
-    Thread.Sleep(500);
+    Thread.Sleep(threadSleepfr);
     Console.WriteLine("FIF");
-    Thread.Sleep(500);
+    Thread.Sleep(threadSleepfr);
     Console.WriteLine("TEEN");
-    Thread.Sleep(500);
+    Thread.Sleep(threadSleepfr);
     Console.WriteLine("names, and you didn't pick a single one.");
-    Thread.Sleep(4000);
+    Thread.Sleep(threadSleepfr*8);
     Console.WriteLine("Have fun in your paradise of nameless heathens.");
-    Thread.Sleep(4500);
+    Thread.Sleep(threadSleepfr*9);
     Console.WriteLine("God damn heretic.");
-    Thread.Sleep(1750);
+    Thread.Sleep(threadSleepfr*3);
 }
 
 else if (yN == "yes") {
     Console.WriteLine("Good.");
-    Thread.Sleep(1000);
+    Thread.Sleep(threadSleepfr*2);
     Console.WriteLine("Then we shall begin.");
-    Thread.Sleep(2000);
+    Thread.Sleep(threadSleepfr*4);
 }
 
 if (pis != pis2) {
     Console.WriteLine($"You are {name}.");
-    Thread.Sleep(1500);
+    Thread.Sleep(threadSleepfr*3);
     Console.WriteLine($"You live in the wonderful and mystical land of Birmingham (all rights reserved)");
-    Thread.Sleep(3500);
+    Thread.Sleep(threadSleepfr*7);
     Console.WriteLine($"Where will you go?");
-    Thread.Sleep(1000);
+    Thread.Sleep(threadSleepfr*2);
     Console.WriteLine($"1. Nowhere");
     Console.WriteLine($"2. Towards Jacksonville");
     Console.WriteLine($"3. Towards Nairobi");
@@ -111,15 +120,15 @@ if (pis != pis2) {
             Console.WriteLine($"+1 Rep. You now have {reputation} Reputation.");
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("You start to treck towards Jacksonville.");
-            Thread.Sleep(2500);
+            Thread.Sleep(threadSleepfr*5);
             Console.WriteLine("...");
-            Thread.Sleep(2000);
+            Thread.Sleep(threadSleepfr*4);
             Console.WriteLine("Suddenly, you see a small child on the side of the road.");
-            Thread.Sleep(2500);
+            Thread.Sleep(threadSleepfr*5);
             Console.WriteLine("They seem hurt.");
-            Thread.Sleep(1500);
+            Thread.Sleep(threadSleepfr*3);
             Console.WriteLine("Do you help them?");
-            Thread.Sleep(1000);
+            Thread.Sleep(threadSleepfr*2);
             Console.WriteLine("1. Of course! It's a child!");
             Console.WriteLine("2. Check on their injuries, decide a course of action from there.");
             Console.WriteLine("3. Move the child. They are in my way.");
@@ -131,73 +140,80 @@ if (pis != pis2) {
                 Console.WriteLine($"+50 Rep. You now have {reputation} Reputation.");
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("You tend to the child's wounds, they stare up at you with a confused look on their face.");
-                Thread.Sleep(3500);
+                Thread.Sleep(threadSleepfr*7);
                 Console.ForegroundColor = ConsoleColor.Gray;
                 Console.WriteLine($"'What's wrong?', you ask.");
                 Console.ForegroundColor = ConsoleColor.White;
-                Thread.Sleep(2500);
+                Thread.Sleep(threadSleepfr*5);
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("'Nothing. I just didn't expect anyone to help.', they said. 'Who are you?'");
                 Console.ForegroundColor = ConsoleColor.White;
-                Thread.Sleep(4500);
+                Thread.Sleep(threadSleepfr*9);
                 if (name == "Miller Freiburg") {
                     Console.ForegroundColor = ConsoleColor.Gray;
                     Console.WriteLine("'Miller, Miller Freiburg Thompson Carlst Thwernthorp Oculus Tavarnus II.', you command.");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Thread.Sleep(5000);
+                    Thread.Sleep(threadSleepfr*10);
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("'Um... can I just... call you 'Miller Freiburg' for short?', they ask.");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Thread.Sleep(5000);
+                    Thread.Sleep(threadSleepfr*10);
                     Console.WriteLine("You stare up for a while, holding your waists as if to prepare for a heroic speech.");
-                    Thread.Sleep(4000);
+                    Thread.Sleep(threadSleepfr*8);
                     Console.WriteLine("The child stares back blankly.");
-                    Thread.Sleep(2000);
+                    Thread.Sleep(threadSleepfr*4);
                     Console.ForegroundColor = ConsoleColor.Gray;
                     Console.WriteLine("'No.'");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Thread.Sleep(2000);
+                    Thread.Sleep(threadSleepfr*4);
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("'Oh.'");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Thread.Sleep(2000);
+                    Thread.Sleep(threadSleepfr*4);
                     Console.WriteLine("...");
-                    Thread.Sleep(5000);
+                    Thread.Sleep(threadSleepfr*10);
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("'Are... you gonna drag me up?', the child requested, still laying dormant on the floor.");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Thread.Sleep(6000);
+                    Thread.Sleep(threadSleepfr*12);
                     Console.ForegroundColor = ConsoleColor.Gray;
                     Console.WriteLine("'Okay', you relented. You reach out a hand and drag the child up, meeting their weight with an accidental headbutt.");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Thread.Sleep(6500);
+                    Thread.Sleep(threadSleepfr*13);
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("'Ow...'");
+                    Thread.Sleep(threadSleepfr*4);
+                    Console.WriteLine("'So... where are you going, then?', the child asked.");
+                    Thread.Sleep(threadSleepfr*8);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("'I forgot.', you tell the child. 'Bye.' You turn around and journey home to your humble abode.");
+                    Thread.Sleep(threadSleepfr*12);
                     Console.ForegroundColor = ConsoleColor.White;
-                    Thread.Sleep(2000);
+                    Console.WriteLine("The child just stands there in confusion.");
+                    Thread.Sleep(threadSleepfr*6);
                 }
                 else {
                     Console.ForegroundColor = ConsoleColor.Gray;
                     Console.WriteLine($"'{name}, {name} Smith.', you reach out your hand. 'And you?'");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Thread.Sleep(3000);
+                    Thread.Sleep(threadSleepfr*6);
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("'Charlie', they respond. Their hand reaching out to meet yours.");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Thread.Sleep(3000);
+                    Thread.Sleep(threadSleepfr*6);
                     Console.WriteLine("You drag them up from the floor. You and Charlie continue the treck towards Jacksonville.");
-                    Thread.Sleep(4000);
+                    Thread.Sleep(threadSleepfr*8);
                     Console.WriteLine("...");
-                    Thread.Sleep(5000);
+                    Thread.Sleep(threadSleepfr*10);
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("'Hey... where are we even going, exactly?', said Charlie");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Thread.Sleep(3000);
+                    Thread.Sleep(threadSleepfr*6);
                     Console.WriteLine("You stop abruptly, scratching your head and staring blankly at Charlie.");
-                    Thread.Sleep(4000);
+                    Thread.Sleep(threadSleepfr*8);
                     Console.ForegroundColor = ConsoleColor.Gray;
                     Console.WriteLine("Er, well...");
-                    Thread.Sleep(1000);
+                    Thread.Sleep(threadSleepfr*2);
                     Console.WriteLine("1 --> 'I'm off to visit family at Jacksonville.'");
                     Console.WriteLine("2 --> 'I'm trying to get to Jacksonville as soon as possible.'");
                     Console.WriteLine("3 --> 'That's none of your business.'");
@@ -212,29 +228,47 @@ if (pis != pis2) {
                     else if (Answer2 == "three") {
                         Answer2 = "3";
                     }
+                    while (Answer2 != "1" && Answer2 != "2" && Answer2 != "3") {
+                        Console.WriteLine("Invalid answer. Please type 1, 2, or 3.");
+                        string rawInAnswer2 = Console.ReadLine();
+                        string inAnswer2 = rawInAnswer2.ToLower();
+                        if (inAnswer2 == "one") {
+                            inAnswer2 = "1";
+                        }
+                        else if (inAnswer2 == "two") {
+                            inAnswer2 = "2";
+                        }
+                        else if (inAnswer2 == "three") {
+                            inAnswer2 = "3";
+                        }
+                        if (inAnswer2 == "1" || inAnswer2 == "2" || inAnswer2 == "3") {
+                            Answer2 = inAnswer2;
+                            break;
+                        }
+                    }
                     if (Answer2 == "1" || Answer2 == "2") {
                         Console.ForegroundColor = ConsoleColor.Blue;
                         Console.WriteLine($"'Jacksonville? {name}, you do know that Jacksonville is across the Atlantic Ocean, right?', said Charlie");
                         Console.ForegroundColor = ConsoleColor.White;
-                        Thread.Sleep(3000);
+                        Thread.Sleep(threadSleepfr*10);
                         Console.WriteLine("You stop abruptly, staring blankly.");
-                        Thread.Sleep(4000);
+                        Thread.Sleep(threadSleepfr*8);
                         Console.WriteLine("You open your mouth as to speak, but stop before even a husk of noise can escape.");
-                        Thread.Sleep(5000);
+                        Thread.Sleep(threadSleepfr*10);
                         Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine($"{name}?");
-                        Thread.Sleep(3000);
+                        Console.WriteLine($"'{name}'?");
+                        Thread.Sleep(threadSleepfr*4);
                         Console.ForegroundColor = ConsoleColor.Gray;
                         Console.WriteLine("'I have to go. Something really important came up.', you tell Charlie, and immediately start running back.");
                         Console.ForegroundColor = ConsoleColor.Blue;
-                        Thread.Sleep(6000);
+                        Thread.Sleep(threadSleepfr*12);
                         Console.WriteLine($"'{name}?!', Charlie screams after you.");
-                        Thread.Sleep(2000);
+                        Thread.Sleep(threadSleepfr*4);
                         Console.WriteLine("'You do know where Jacksonville (...)'");
-                        Thread.Sleep(2000);
+                        Thread.Sleep(threadSleepfr*4);
                         Console.ForegroundColor = ConsoleColor.White;
                         Console.WriteLine("The voice blends in with the surrounding noise, becoming naught but a whisper in the sea of voices.");
-                        Thread.Sleep(6000);
+                        Thread.Sleep(threadSleepfr*12);
                         reputation = reputation-1;
                     }
                 }
@@ -243,21 +277,21 @@ if (pis != pis2) {
     }
 }
 
-if (reputation == 50) {
+if (reputation == 50 || name == "Miller Freiburg") {
     Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("You close the door to your humble abode.");
-    Thread.Sleep(2000);
+    Thread.Sleep(threadSleepfr*4);
     Console.WriteLine("Your journey's finally come to an end.");
-    Thread.Sleep(2000);
+    Thread.Sleep(threadSleepfr*4);
     Console.WriteLine("Unluckily, you didn't manage to reach your location.");
-    Thread.Sleep(2000);
+    Thread.Sleep(threadSleepfr*4);
     Console.WriteLine("But, perhaps the true Journey were the friends we made along the way.");
-    Thread.Sleep(2000);
+    Thread.Sleep(threadSleepfr*4);
     Console.WriteLine("Or maybe not.");
-    Thread.Sleep(2000);
+    Thread.Sleep(threadSleepfr*4);
     Console.WriteLine("Maybe you're just stupid.");
-    Thread.Sleep(2000);
-    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Thread.Sleep(threadSleepfr*4);
+    Console.ForegroundColor = ConsoleColor.Cyan;
     Console.WriteLine("DESTINATION OUT OF REACH ENDING");
     Console.ReadLine();
 }
